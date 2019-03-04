bd1395c37b46ad3aac75fb8e899d81f0464624b4
Olívia Mattiazzo <Olivia.Mattiazzo@ctr.unipartner.com>
2019-03-04 17:18:09 +0000

    Adjustments

--- js/events.js
+++ js/events.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
 })
-.keypress(function(e) {
-    if(e.which == 13) $('#btnAdd').click();
-});
+    .keypress(function (e) {
+        if (e.which == 13) $('#btnAdd').click();
+    });
 
 var btnGenerate = document.getElementById("btnGenerate");
-btnGenerate.addEventListener("click", function(){
+btnGenerate.addEventListener("click", function () {
     countryChart.destroy();
 });
 
-$("#btnAdd").click(function() {
+$("#btnAdd").click(function () {
     var addedTitle = $("#title").val();
     if (!addedTitle) {
         alert("Title is mandatory!");
@@ -20,11 +20,55 @@ $("#btnAdd").click(function() {
     searchMovieInfo(addedTitle);
 });
 
-$("#btnGenerate").click(function() {
+$("#btnGenerate").click(function () {
     countContryVotes();
     generateCountryChart();
 });
 
-$("#btnDownloadXml").click(function() {
+$("#btnDownloadXml").click(function () {
     downloadsXml();
+});
+
+$("#btnUpload").click(function () {
+    var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xml)$/;
+    if (regex.test($("#fileUpload").val().toLowerCase())) {
+        if (typeof (FileReader) != "undefined") {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var xmlDoc = $.parseXML(e.target.result);
+                var customers = $(xmlDoc).find("Customer");
+
+                //Create a HTML Table element.
+                var table = $("<table />");
+                table[0].border = "1";
+
+                //Add the header row.
+                var row = $(table[0].insertRow(-1));
+                customers.eq(0).children().each(function () {
+                    var headerCell = $("<th />");
+                    headerCell.html(this.nodeName);
+                    row.append(headerCell);
+                });
+
+                //Add the data rows.
+                $(customers).each(function () {
+                    row = $(table[0].insertRow(-1));
+                    $(this).children().each(function () {
+                        var cell = $("<td />");
+                        cell.html($(this).text());
+                        row.append(cell);
+                    });
+                });
+
+                var dvTable = $("#dvTable");
+                dvTable.html("");
+                dvTable.append(table);
+            }
+            reader.readAsText($("#fileUpload")[0].files[0]);
+        } else {
+            alert("This browser does not support HTML5.");
+        }
+    } else {
+        alert("Please upload a valid XML file.");
+    }
 });
