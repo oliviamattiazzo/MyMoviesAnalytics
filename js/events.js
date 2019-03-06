f40bfe1df3a91b2396f0be0aab6ca2317dcdb9ce
Olívia Mattiazzo <Olivia.Mattiazzo@ctr.unipartner.com>
2019-03-06 15:09:39 +0000

    Ended the XML importation development

--- js/events.js
+++ js/events.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 })
-.keypress(function (e) {
-    if (e.which == 13) $('#btnAdd').click();
-});
+    .keypress(function (e) {
+        if (e.which == 13) $('#btnAdd').click();
+    });
 
 var btnAdd = document.getElementById("btnAdd");
 btnAdd.addEventListener("click", function () {
@@ -24,27 +24,15 @@ $("#btnDownloadXml").click(function () {
     downloadsXml();
 });
 
-$("#btnUploadXml").click(function(){
+$("#btnUploadXml").click(function () {
     $("#inpXmlFile").click();
 });
 
 $("#inpXmlFile").change(function () {
-    var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xml)$/;
-
-    if (regex.test($("#inpXmlFile").val().toLowerCase())) {
-        if (typeof (FileReader) != "undefined") {
-            var reader = new FileReader();
-            
-            reader.readAsText($("#inpXmlFile")[0].files[0]);
-            var xmlDoc = $.parseXML(reader.result);
-            var movies = $(xmlDoc).find("movies").find("title");
-
-            for (var i=0; i < movies.length; i++) {
-                searchMovieInfo(movies[i].innerHTML);
-            }
-        } else {
-            alert("This browser does not support HTML5.");
-        }
+    var fileUploaded = $("#inpXmlFile")[0].files[0];
+    
+    if (validatesXmlExtension($("#inpXmlFile").val().toLowerCase())) {
+        importXml(fileUploaded);
     } else {
         alert("Please upload a valid XML file.");
     }
--- js/xmlmanipulation.js
+++ js/xmlmanipulation.js
@@ -15,15 +15,38 @@ function createXmlText() {
 function downloadsXml() {
     var xmlText = createXmlText();
     var filename = "mymovies.xml";
-    var bb = new Blob([xmlText], {type: 'text/plain'});
-    
+    var bb = new Blob([xmlText], { type: 'text/plain' });
+
     var pom = document.createElement('a');
     pom.setAttribute('href', window.URL.createObjectURL(bb));
     pom.setAttribute('download', filename);
-    
+
     pom.dataset.downloadurl = ['text/plain', pom.download, pom.href].join(':');
-    pom.draggable = true; 
+    pom.draggable = true;
     pom.classList.add('dragout');
 
     pom.click();
+}
+
+function importXml(fileUploaded) {
+    if (typeof (FileReader) != "undefined") {
+        var reader = new FileReader();
+
+        reader.onload = function () {
+            var xmlDoc = $.parseXML(reader.result);
+            var movies = $(xmlDoc).find("movies").find("title");
+
+            for (var i = 0; i < movies.length; i++) {
+                searchMovieInfo(movies[i].innerHTML);
+            }
+        };
+        reader.readAsText(fileUploaded);
+    } else {
+        alert("This browser does not support HTML5.");
+    }
+}
+
+function validatesXmlExtension(fileName) {
+    var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.xml)$/;
+    return regex.test(fileName);
 }
