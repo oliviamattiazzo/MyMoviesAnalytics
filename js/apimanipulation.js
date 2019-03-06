cb6a1df62a720bbf99d2cf8336eabd9cdd7209f5
Olívia Mattiazzo <Olivia.Mattiazzo@ctr.unipartner.com>
2019-03-06 11:47:53 +0000

    Implemented a way to upload the XML, but there are some bugs

--- js/apimanipulation.js
+++ js/apimanipulation.js
@@ -7,10 +7,10 @@ function searchMovieInfo(addedTitle) {
         }
         else {
             addLine(addedTitle);
-            showsTable();
             arrTitleInfo.push(result);
-
-            generateCountryChart();
         }
+    }).done(function() {
+        showsTable();
+        generateCountryChart();
     });
 }
--- js/events.js
+++ js/events.js
@@ -34,37 +34,14 @@ $("#inpXmlFile").change(function () {
     if (regex.test($("#inpXmlFile").val().toLowerCase())) {
         if (typeof (FileReader) != "undefined") {
             var reader = new FileReader();
-            reader.onload = function (e) {
-                var xmlDoc = $.parseXML(e.target.result);
-                var customers = $(xmlDoc).find("Customer");
+            
+            reader.readAsText($("#inpXmlFile")[0].files[0]);
+            var xmlDoc = $.parseXML(reader.result);
+            var movies = $(xmlDoc).find("movies").find("title");
 
-                //Create a HTML Table element.
-                var table = $("<table />");
-                table[0].border = "1";
-
-                //Add the header row.
-                var row = $(table[0].insertRow(-1));
-                customers.eq(0).children().each(function () {
-                    var headerCell = $("<th />");
-                    headerCell.html(this.nodeName);
-                    row.append(headerCell);
-                });
-
-                //Add the data rows.
-                $(customers).each(function () {
-                    row = $(table[0].insertRow(-1));
-                    $(this).children().each(function () {
-                        var cell = $("<td />");
-                        cell.html($(this).text());
-                        row.append(cell);
-                    });
-                });
-
-                var dvTable = $("#dvTable");
-                dvTable.html("");
-                dvTable.append(table);
+            for (var i=0; i < movies.length; i++) {
+                searchMovieInfo(movies[i].innerHTML);
             }
-            reader.readAsText($("#fileUpload")[0].files[0]);
         } else {
             alert("This browser does not support HTML5.");
         }
