8c621fefef16f27a5eeeef7a3438549561a06a0a
Olivia Mattiazzo <oli.pmatt@gmail.com>
2019-03-23 17:28:54 +0000

    Added time processing (only minutes >> days)

--- js/countersmanipulation.js
+++ js/countersmanipulation.js
@@ -14,5 +14,22 @@ function setValueTimeWatchingMoviesCounter() {
         totalTime += parseInt(element);
     });
 
-    $("#watched-movies-time").html(totalTime + " min");
+    $("#watched-movies-time").html(timeProcessor(totalTime));
+}
+
+function timeProcessor(timeInMinutes) {
+    if (timeInMinutes < 60)
+        return timeInMinutes  + " minutes";
+
+    const hoursInDecimal = timeInMinutes / 60;
+    const hoursInInt = parseInt(hoursInDecimal);
+
+    if (hoursInInt < 24)
+        return minutesToHourConverter(hoursInDecimal, hoursInInt);
+}
+
+function minutesToHourConverter(hoursInDecimal, hoursInInt) {
+    const minutes = parseInt( (hoursInDecimal - hoursInInt) * 60 );
+    
+    return hoursInInt + " hours and " + minutes + " minutes";
 }
